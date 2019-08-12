786edab47c9a238171a44ab0ebf543b8a8bf8e59
Baiburin Valery <otpt@yandex.ru>
2019-08-12 13:11:00 +0900

    Create 1051.cpp

---
+++ 1051.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> sorted_heights(heights);
+        sort(sorted_heights.begin(), sorted_heights.end());
+        
+        int result = 0;
+        for (int i = 0; i < heights.size(); ++i) {
+            result += heights[i] != sorted_heights[i];
+        }
+        return result;
+    }
+};
