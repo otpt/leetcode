ccbfa63d29adf3fed47d12e6abe87db8537a90c0
Baiburin Valery <otpt@yandex.ru>
2019-08-19 23:40:36 +0900

    Create 893.cpp

---
+++ 893.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSpecialEquivGroups(vector<string>& A) {
+        unordered_set<string> classes;
+        
+        for (auto & s : A) {
+            string odd;
+            string even;
+            
+            int d = 0;
+            for(auto & ch : s) {
+                if (d) {
+                    odd += ch;
+                } else {
+                    even += ch;
+                }
+                d = 1 - d;
+            }
+            sort(odd.begin(), odd.end());
+            sort(even.begin(), even.end());
+            classes.insert(odd + even);
+        }
+        
+        return classes.size();
+    }
+};
