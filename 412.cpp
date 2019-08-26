abd87972db5af06353eace9592e0eae4c3cc3459
Baiburin Valery <otpt@yandex.ru>
2019-08-26 22:50:50 +0900

    Create 412.cpp

---
+++ 412.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+        for (int i = 1; i <= n; ++i) {
+            if (i % 15 == 0) {
+                result.push_back("FizzBuzz");
+                continue;
+            }
+            if (i % 3 == 0) {
+                result.push_back("Fizz");
+                continue;
+            }
+            if (i % 5 == 0) {
+                result.push_back("Buzz");
+                continue;
+            }
+            result.push_back(to_string(i));
+        }
+        return result;
+    }
+};
